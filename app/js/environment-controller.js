dd6fa36b6e16ad6efbedc35e8d6f3ef170859397
Tim Rourke <tim@timrourke.com>
2018-09-12 16:36:16 -0500

    Implement first 4 requirements

--- app/js/environment-controller.js
+++ app/js/environment-controller.js
@@ -1,14 +1,68 @@
-window.helloText = function() {
-  return 'Hello, World!';
-};
+function Hvac(initialTemperature) {
+  this.temperature = initialTemperature;
+  this.coolCalledWith = [];
+  this.heatCalledWith = [];
+  this.fanCalledWith = [];
 
-window.hello = function() {
-  html = JST['app/templates/hello.us']({text: helloText()});
-  document.body.innerHTML += html;
-};
+  this.cool = function cool(shouldCool) {
+    this.coolCalledWith.push(shouldCool);
+  }
 
-if(window.addEventListener) {
-  window.addEventListener('DOMContentLoaded', hello, false);
-} else {
-  window.attachEvent('onload', hello);
+  this.heat = function heat(shouldHeat){
+    this.heatCalledWith.push(shouldHeat);
+  }
+
+  this.fan = function fan(shouldFanBeSet){
+    this.fanCalledWith.push(shouldFanBeSet);
+  }
+
+  this.temp = function temp() {
+    return this.temperature;
+  }
+}
+
+function EnvironmentController(hvac) {
+  this.hvac = hvac;
+  this.fanPreviousStates = [false];
+
+  this.tick = function tick() {
+    if (this.hvac.temp() > 75) {
+      this.hvac.cool(true);
+      this.hvac.heat(false);
+      this.hvac.fan(this.shouldTurnFanOn(this.hvac.temp()));
+    }
+    else if (this.hvac.temp() < 65){
+      this.hvac.cool(false);
+      this.hvac.heat(true);
+      this.hvac.fan(this.shouldTurnFanOn(this.hvac.temp()));
+    }
+    else {
+      this.hvac.cool(false);
+      this.hvac.heat(false);
+      this.hvac.fan(this.shouldTurnFanOn(this.hvac.temp()));
+    }
+  }
+
+  this.shouldTurnFanOn = function shouldTurnFanOn(temperature) {
+    let nextState = false;
+    let shouldFanNormallyTurnOn = temperature < 65 || temperature > 75;
+    let numMinutesFanLastOn = this.fanPreviousStates
+      .reverse()
+      .indexOf(true) + 1;
+
+    let fanWasOnWithinLast5Minutes = numMinutesFanLastOn > 0 &&
+      numMinutesFanLastOn < 5;
+
+    if (!fanWasOnWithinLast5Minutes && shouldFanNormallyTurnOn) {
+      nextState = true;
+    }
+
+    this.fanPreviousStates.push(nextState);
+
+    return nextState;
+  }
+}
+
+function createStubHvac(initialTemperature) {
+  return new Hvac(initialTemperature);
 }
--- spec/environment-controller-spec.js
+++ spec/environment-controller-spec.js
@@ -1,4 +1,55 @@
-describe(".helloText", function(){
-  When(function(){ this.result = helloText(); });
-  Then(function(){ expect(this.result).toEqual("Hello, World!"); });
+describe("turns on cooling and fan when temperature > 75", function() {
+  When(function() {
+    this.stubHvac = createStubHvac(76);
+    this.environmentController = new EnvironmentController(this.stubHvac);
+    this.environmentController.tick();
+  });
+  Then(function() {
+    expect(this.stubHvac.coolCalledWith[0]).toEqual(true);
+    expect(this.stubHvac.heatCalledWith[0]).toEqual(false);
+    expect(this.stubHvac.fanCalledWith[0]).toEqual(true);
+  });
+
+});
+
+describe("turns on heat and fan when temparature < 65", function() {
+  When(function() {
+    this.stubHvac = createStubHvac(64);
+    this.environmentController = new EnvironmentController(this.stubHvac);
+    this.environmentController.tick();
+  });
+  Then(function() {
+    expect(this.stubHvac.coolCalledWith[0]).toEqual(false);
+    expect(this.stubHvac.heatCalledWith[0]).toEqual(true);
+    expect(this.stubHvac.fanCalledWith[0]).toEqual(true);
+  });
+});
+
+describe("does not call cool, heat, or fan when temperature between 65 and 75", function() {
+  When(function() {
+    this.stubHvac = createStubHvac(70);
+    this.environmentController = new EnvironmentController(this.stubHvac);
+    this.environmentController.tick();
+  });
+  Then(function() {
+    expect(this.stubHvac.coolCalledWith[0]).toEqual(false);
+    expect(this.stubHvac.heatCalledWith[0]).toEqual(false);
+    expect(this.stubHvac.fanCalledWith[0]).toEqual(false);
+  });
+});
+describe("heater is currently on, when heater is turned off, fan can't run for 5 minutes", function(){
+  When(function() {
+    this.stubHvac = createStubHvac(64);
+    this.environmentController = new EnvironmentController(this.stubHvac);
+    this.environmentController.tick();
+    this.environmentController.tick();
+    this.environmentController.tick();
+    this.environmentController.tick();
+    this.environmentController.tick();
+    this.environmentController.tick();
+    this.environmentController.tick();
+  });
+  Then(function() {
+    expect(this.stubHvac.fanCalledWith).toEqual([true, false, false, false, false, false, true]);
+  });
 });
